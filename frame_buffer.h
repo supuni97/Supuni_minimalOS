1fef2394982d7669e1a0d38552df8fe255f6770a
Supuni Sithara <73241855+supuni97@users.noreply.github.com>
2021-08-27 14:21:50 +0530

    Add files via upload

--- frame_buffer.h
+++ frame_buffer.h
@@ -1,25 +1,33 @@
-#include "io.h"
+#ifndef INCLUDE_FRAMEBUFFER_H
+    #define INCLUDE_FRAMEBUFFER_H
+    #include "io.h"
 
-#define FB_COMMAND_PORT         0x3D4
-#define FB_DATA_PORT            0x3D5
+    /* The I/O ports */
+    #define FB_COMMAND_PORT         0x3D4
+    #define FB_DATA_PORT            0x3D5
 
-#define FB_HIGH_BYTE_COMMAND    14
-#define FB_LOW_BYTE_COMMAND     15
+    /* The I/O port commands */
+    #define FB_HIGH_BYTE_COMMAND    14
+    #define FB_LOW_BYTE_COMMAND     15
 
-void fb_move_cursor(unsigned short pos);
+    
+    #define FB_GREEN     2
+    #define FB_DARK_GREY 8
+    #define FB_BLACK     0
+    #define FB_BLUE      1
+    #define FB_GREEN     2
+    #define FB_RED       4
+    #define FB_DARK_GREY 8
+    #define FB_WHITE     15
 
-#define FB_BLACK     0
-#define FB_BLUE      1
-#define FB_GREEN     2
-#define FB_RED       4
-#define FB_DARK_GREY 8
-#define FB_WHITE     15
-#define FRAMEBUFFER_ADDRESS 0x000B8000
+    
+     void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg);
+    
+     void fb_move_cursor(unsigned short pos);
+    
+     int fb_write(char *buf, unsigned int len);
+    
+    #endif
+    
 
-void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg);
 
-void fb_write_simple();
-
-int fb_write(char *buf, unsigned int len);
-
-void fb_clear();
--- pic.c
+++ pic.c
@@ -1,6 +1,18 @@
 #include "io.h"
 #include "pic.h"
 
+/*
+        Programmable Interrupt Controller
+	From: http://wiki.osdev.org/PIC
+	Reinitialize the PIC controllers, giving them specified vector offsets
+	rather than 8h and 70h, as configured by default.
+*/
+
+/**
+  *  Acknowledges an interrupt from either PIC 1 or PIC 2.
+  *
+  *  @param num The number of the interrupt
+  */
 void pic_acknowledge(unsigned int interrupt)
 {
 	if (interrupt < PIC_1_OFFSET || interrupt > PIC_2_END) {
@@ -14,7 +26,12 @@ void pic_acknowledge(unsigned int interrupt)
 	}
 }
 
-
+/*
+arguments:
+	offset1 - vector offset for master PIC
+		vectors on the master become offset1..offset1+7
+	offset2 - same for slave PIC: offset2..offset2+7
+*/
 void pic_remap(int offset1, int offset2)
 {
 	outb(PIC_1_COMMAND, PIC_ICW1_INIT + PIC_ICW1_ICW4);	// starts the initialization sequence (in cascade mode)
@@ -24,9 +41,10 @@ void pic_remap(int offset1, int offset2)
 	outb(PIC_1_DATA, 4);					// ICW3: tell Master PIC that there is a slave PIC at IRQ2 (0000 0100)
 	outb(PIC_2_DATA, 2);					// ICW3: tell Slave PIC its cascade identity (0000 0010)
 
-outb(PIC_1_DATA, PIC_ICW4_8086);
+	outb(PIC_1_DATA, PIC_ICW4_8086);
 	outb(PIC_2_DATA, PIC_ICW4_8086);
 
+        // Setup Interrupt Mask Register (IMR)
 	outb(PIC_1_DATA, 0xFD); // 1111 1101 - Enable IRQ 1 only (keyboard).
 	outb(PIC_2_DATA, 0xFF);
 
