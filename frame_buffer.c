0d90561d4e0ae52fa2273b7470dd9035470356fd
Supuni Sithara <73241855+supuni97@users.noreply.github.com>
2021-08-27 19:42:29 +0530

    Add files via upload (#13)

--- frame_buffer.c
+++ frame_buffer.c
@@ -1,51 +1,31 @@
 #include "io.h"
 #include "frame_buffer.h"
 
-
-unsigned short __fb_present_pos = 0x00000000;
-
-void fb_move_cursor(unsigned short pos)
-{
-	outb(FB_COMMAND_PORT, FB_HIGH_BYTE_COMMAND);
+     char *fb = (char *) 0x000B8000;
+     unsigned int writing_state = 800;
+     unsigned int cursor_state=400;
+    void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg)
+    {
+        fb[i] = c;
+        fb[i + 1] = ((fg & 0x0F) << 4) | (bg & 0x0F);
+    }
+    
+        void fb_move_cursor(unsigned short pos)
+    {
+        outb(FB_COMMAND_PORT, FB_HIGH_BYTE_COMMAND);
         outb(FB_DATA_PORT,    ((pos >> 8) & 0x00FF));
         outb(FB_COMMAND_PORT, FB_LOW_BYTE_COMMAND);
         outb(FB_DATA_PORT,    pos & 0x00FF);
-	__fb_present_pos = pos;
-}
-
-
-void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg)
-{
-	char *fb = (char *) FRAMEBUFFER_ADDRESS;
-	fb[i] = c;
-        fb[i + 1] = ((fg & 0x0F) << 4) | (bg & 0x0F);
-}
-
-void fb_write_simple()
-{
-	fb_write_cell(0, 'T', FB_GREEN, FB_DARK_GREY);
-}
-
-
-int fb_write(char *buf, unsigned int len) {
-	
-	unsigned int i;
-	for(i=0; i<len; i++) {
-		
-		fb_write_cell(2*__fb_present_pos, buf[i], FB_GREEN, FB_DARK_GREY);
-		fb_move_cursor(__fb_present_pos + 1);
-		
-		}		
-	return len;
-}
-
-
-void fb_clear(){
-  
-  unsigned int i=0;
-  while(i<80*25*2) {
-  	    fb_write_cell(2*i, ' ', FB_BLACK, FB_BLACK);
-  	    i=i+1;
-  }
-  return;
-}
+    }
+    
+    int fb_write(char *buf, unsigned int len){
+	    for (unsigned int i=0; i<len;i++){
+	   
+	      fb_write_cell((writing_state+(i*2)), *(buf+i), FB_WHITE, FB_GREEN);
+	   	 
+	   	 }
+		writing_state+=len*2;
+		cursor_state+=len;
+		fb_move_cursor(cursor_state);
+	   	 return 0;
+    }
