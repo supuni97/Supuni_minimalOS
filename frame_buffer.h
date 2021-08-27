c8d2349791c27ba7901350bdd9225755b6d538e2
Supuni Sithara <73241855+supuni97@users.noreply.github.com>
2021-08-27 19:07:07 +0530

    Revert "Add files via upload (#9)"
    This reverts commit f46faea47a5c0a0c6c465e15042f2aebb16a7d13.

--- frame_buffer.h
+++ frame_buffer.h
@@ -1,33 +1,25 @@
-#ifndef INCLUDE_FRAMEBUFFER_H
-    #define INCLUDE_FRAMEBUFFER_H
-    #include "io.h"
+#include "io.h"
 
-    /* The I/O ports */
-    #define FB_COMMAND_PORT         0x3D4
-    #define FB_DATA_PORT            0x3D5
+#define FB_COMMAND_PORT         0x3D4
+#define FB_DATA_PORT            0x3D5
 
-    /* The I/O port commands */
-    #define FB_HIGH_BYTE_COMMAND    14
-    #define FB_LOW_BYTE_COMMAND     15
+#define FB_HIGH_BYTE_COMMAND    14
+#define FB_LOW_BYTE_COMMAND     15
 
-    
-    #define FB_GREEN     2
-    #define FB_DARK_GREY 8
-    #define FB_BLACK     0
-    #define FB_BLUE      1
-    #define FB_GREEN     2
-    #define FB_RED       4
-    #define FB_DARK_GREY 8
-    #define FB_WHITE     15
+void fb_move_cursor(unsigned short pos);
 
-    
-     void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg);
-    
-     void fb_move_cursor(unsigned short pos);
-    
-     int fb_write(char *buf, unsigned int len);
-    
-    #endif
-    
+#define FB_BLACK     0
+#define FB_BLUE      1
+#define FB_GREEN     2
+#define FB_RED       4
+#define FB_DARK_GREY 8
+#define FB_WHITE     15
+#define FRAMEBUFFER_ADDRESS 0x000B8000
 
+void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg);
 
+void fb_write_simple();
+
+int fb_write(char *buf, unsigned int len);
+
+void fb_clear();
